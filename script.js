c8d90fde0813d804061f0107489df7ee22d440df
Guilherme <guilhermeccintra@hotmail.com>
2026-09-09 16:32:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -927,43 +927,6 @@ document.addEventListener(
         }
 
 
-       
-         /*
-         * BARRA TOPO
-         */
-
-       function atualizarDataPromocao() {
-    const elemento = document.getElementById("promo-date");
-
-    if (!elemento) return;
-
-    const hoje = new Date();
-
-    const dia = String(hoje.getDate()).padStart(2, "0");
-
-    const meses = [
-        "JANEIRO",
-        "FEVEREIRO",
-        "MARÇO",
-        "ABRIL",
-        "MAIO",
-        "JUNHO",
-        "JULHO",
-        "AGOSTO",
-        "SETEMBRO",
-        "OUTUBRO",
-        "NOVEMBRO",
-        "DEZEMBRO"
-    ];
-
-    const mes = meses[hoje.getMonth()];
-    const ano = hoje.getFullYear();
-
-    elemento.textContent = `${dia} DE ${mes} DE ${ano}!`;
-}
-
-atualizarDataPromocao();
-
 
 
         /*
